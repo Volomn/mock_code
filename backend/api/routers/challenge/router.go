34e78aa9d0b50b6b733c11ee920c3de0f6651ae1
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2023-08-01 22:59:44 +0100

    define endpoint to get challenge

--- backend/api/routers/challenge/router.go
+++ backend/api/routers/challenge/router.go
@@ -11,6 +11,7 @@ func GetChallengeRouter() chi.Router {
 	var router = chi.NewRouter()
 	router.Post("/", middleware.AuthenticationMiddleware(middleware.AuthorizationMiddleWare(false, true)(http.HandlerFunc(AddChallenge))).(http.HandlerFunc))
 	router.Get("/", FetchChallenges)
+	router.Get("/{challengeId}", GetChallenge)
 	router.Post("/{challengeId}/input", middleware.AuthenticationMiddleware(middleware.AuthorizationMiddleWare(false, true)(http.HandlerFunc(UploadInputFile))).(http.HandlerFunc))
 	router.Post("/{challengeId}/open", middleware.AuthenticationMiddleware(middleware.AuthorizationMiddleWare(false, true)(http.HandlerFunc(OpenChallenge))).(http.HandlerFunc))
 	return router
